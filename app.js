8cb6ac70cea6d3712cf2fa9a75cb7d7b06ad0464
Bogdan <89359664+BoggoC@users.noreply.github.com>
2022-05-13 00:03:37 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+// dropdown bottom
+function myFunc() {
+  document.getElementById("myDropdown-bottom").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn-bottom")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content-bottom");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+// FAQ Functionality
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  // console.log(question);
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
